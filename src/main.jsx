3679ec793b51e540ab85f762f67050933cf80f2b
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-09-14 13:54:59 +0500

    donee

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-// import 'react-tabs/style/react-tabs.css'
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 		<>
 			<HeroComponent />
 			<AdvantagesComponent />
-			<TabsComponent />
+			{/* <TabsComponent /> */}
 		</>
 	)
 }
