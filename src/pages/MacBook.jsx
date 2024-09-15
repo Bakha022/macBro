faee4c6f23f812fee172b0f5a66e2c0919becab2
Bakha022 <abduqahhorovbahodir536@gmail.com>
2024-09-15 19:08:47 +0500

    done!

--- src/pages/MacBook.jsx
+++ src/pages/MacBook.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
 
+import img from '../assets/mac/mac_4.png'
 const MacBook = () => {
 	return (
-		<div>MacBook</div>
+		<div className='container'>
+			<div className='row'>
+				<div className='col-3'>
+					<div className='card'>
+						<span className='action'>9%</span>
+						<div className='card-img'>
+							<img src={img} alt='mac-img' />
+						</div>
+						<p>MacBook Pro</p>
+					</div>
+					<p className='hello-text'>
+						Add to Cart
+					</p>
+				</div>
+				<div className='col-3'>
+					<div className='card'>
+						<span className='action'>9%</span>
+						<div className='card-img'>
+							<img src={img} alt='mac-img' />
+						</div>
+						<p>MacBook Pro</p>
+					</div>
+					<p className='hello-text'>
+						Add to Cart
+					</p>
+				</div>
+				<div className='col-3'>
+					<div className='card'>
+						<span className='action'>9%</span>
+						<div className='card-img'>
+							<img src={img} alt='mac-img' />
+						</div>
+						<p>MacBook Pro</p>
+					</div>
+					<p className='hello-text'>
+						Add to Cart
+					</p>
+				</div>
+				<div className='col-3'>
+					<div className='card'>
+						<span className='action'>9%</span>
+						<div className='card-img'>
+							<img src={img} alt='mac-img' />
+						</div>
+						<p>MacBook Pro</p>
+					</div>
+					<p className='hello-text'>
+						Add to Cart
+					</p>
+				</div>
+			</div>
+		</div>
 	)
 }
 
